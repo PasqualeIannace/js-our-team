aae604f16b0a642dfe451cb5a227e6ebaeb1e809
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-10 17:13:29 +0100

    MILESTONE 1

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
-const staff = {
-    wayne: {
+const staff = [
+    {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
         img: "wayne-barnett-founder-ceo.jpg"
     },
 
-    angelaC: {
+    {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
         img: "angela-caroll-chief-editor.jpg"
     },
 
-    walter: {
+    {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
         img: "walter-gordon-office-manager.jpg"
     },
 
-    angelaL: {
+    {
         nome: "Angela Lopez",
         ruolo: "Founder & CEO",
         img: "angela-lopez-social-media-manager.jpg"
     },
 
-    scott: {
+    {
         nome: "Scott Estrada",
         ruolo: "Developer",
         img: "scott-estrada-developer.jpg"
     },
 
-    barbara: {
+    {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
         img: "barbara-ramos-graphic-designer.jpg"
     }
+]
+
+for(i = 0; i < staff.length; i++) {
+    console.log(`Nome: ${staff[i].nome}, Ruolo: ${staff[i].ruolo}, srcImg: ${staff[i].img}`);
 }
