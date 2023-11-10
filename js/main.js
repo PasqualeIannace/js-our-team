08db497803679f2bd306ca95d7224fae8ee564f7
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-10 17:04:04 +0100

    milestone 0

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const staff = {
+    wayne: {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    angelaC: {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+
+    walter: {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+
+    angelaL: {
+        nome: "Angela Lopez",
+        ruolo: "Founder & CEO",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+
+    scott: {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+
+    barbara: {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    }
+}
